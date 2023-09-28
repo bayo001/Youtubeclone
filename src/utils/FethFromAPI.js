48407893c3e25cd2aec94c3927f841bb607d07da
bayo <abdulwahababdullahi064@gmail.com>
2023-09-27 17:13:51 -0700

    update api

--- src/utils/FethFromAPI.js
+++ src/utils/FethFromAPI.js
@@ -1,24 +1,26 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+// export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
-const options = {
-  url: BASE_URL,
-  params: {
-    part: 'snippet',
-    videoId: 'M7FIvfx5J10'
-  },
-  headers: {
-    'X-RapidAPI-Key':  process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-  }
-};
+// const options = {
+//   url: BASE_URL,
+//   params: {
+//     part: 'snippet',
+//     videoId: 'M7FIvfx5J10'
+//   },
+//   headers: {
+//     'X-RapidAPI-Key':  process.env.REACT_APP_RAPID_API_KEY,
+//     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+//   }
+// };
 
-export const  FethFromAPI = async (url) => {
-  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+// export const  FethFromAPI = async (url) => {
+//   const { data } = await axios.get(`${BASE_URL}/${url}`, options);
   
-  return data;
-}
+//   return data;
+// }
+
+
 
 
 
